19288099c91a25b3ccd01992fb17e87a6351f3f7
tkrah <thomas.krahn@esailors.de>
2014-10-20 13:12:16 +0200

    Remove setup of Openstack Repository and NTP daemon as this will be done by Ansible

--- common.sh
+++ common.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-yum install -y http://repos.fedorapeople.org/repos/openstack/openstack-juno/rdo-release-juno-1.noarch.rpm
-#yum install -y http://ftp-stud.hs-esslingen.de/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+yum install -y http://ftp-stud.hs-esslingen.de/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 yum update -y
 
 sed -i 's/^127\.0/#127\.0/g' /etc/hosts
 
 systemctl stop firewalld
 systemctl disable firewalld
-
-yum install -y ntp
-systemctl enable ntpd
-systemctl start ntpd
