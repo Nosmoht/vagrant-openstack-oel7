2dd8cc209aa86839ed9c4e7eb9bf6dc41d26ec2a
tkrah <thomas.krahn@esailors.de>
2014-10-25 18:59:42 +0200

    Remove EPEL repo installation as well as system update. Both steps are now included into Ansible role os-common

--- common.sh
+++ common.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-yum install -y http://ftp-stud.hs-esslingen.de/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
-yum update -y
-
 sed -i 's/^127\.0/#127\.0/g' /etc/hosts
 
 systemctl stop firewalld
